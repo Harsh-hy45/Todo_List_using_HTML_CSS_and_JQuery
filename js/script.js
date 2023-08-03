ed2fdcc17173ac32eb6c09748958b497336da780
Harsh-hy45 <68816204+Harsh-hy45@users.noreply.github.com>
2023-08-03 17:06:39 +0530

    commit 3

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
 $(document).ready(function () {
-    
+
+    // Get the form and input elements
     const form = $("#new-task-form");
     const inputTitle = $("#task-title");
     const inputDescription = $("#task-description");
     const inputCompleteByDate = $("#complete-by-date");
     const list_element = $("#tasks");
+
+    // Load existing tasks from localStorage or initialize an empty array
     let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+    // Handle form submission
     form.submit(function (e) {
         e.preventDefault();
+
+        // Get input values
         const title = inputTitle.val();
         const description = inputDescription.val();
         const completeByDate = inputCompleteByDate.val();
-        
+
         // Check if we are editing an existing task
         if (form.data("edit-index") !== undefined) {
-            form.find("#new-task-submit").val("Save Task").prop("disabled", false)
+
+            $(".edit").show();
+
+            $("#new-task-submit").val("Add Task");
+
+            // Get the index of the task being edited
             const editIndex = form.data("edit-index");
+
             updateTask(editIndex, title, description, completeByDate);
-            form.data("edit-index", undefined); // Reset edit-index
-            form.find("#new-task-submit").val("Add Task").prop("disabled", false); // Change button text back to "Add Task"
-            form.find(".edit").show(); // Show the "Edit" button again
+
+            form.data("edit-index", undefined);
+
+
+            form.find(".edit").show();
+
+            // Refresh the page after saving
+            location.reload();
         } else {
+            // If not editing, add a new task
             addTask(title, description, completeByDate);
             tasks.push({
                 title,
@@ -29,17 +47,22 @@ $(document).ready(function () {
                 completeByDate
             });
         }
-        
+
+        // Update local storage with the updated tasks
         updateLocalStorageTasks();
+
+        // Reset input fields after submission
         inputTitle.val("");
         inputDescription.val("");
         inputCompleteByDate.val("");
     });
 
+    // Add existing tasks to the UI
     for (const task of tasks) {
         addTask(task.title, task.description, task.completeByDate);
     }
 
+    // Function to add a new task to the UI
     function addTask(title, description, completeByDate) {
         const task_element = $("<div>").addClass("task");
         const task_content_element = $("<div>").addClass("content");
@@ -66,32 +89,45 @@ $(document).ready(function () {
         task_element.append(task_actions_el);
         list_element.append(task_element);
 
+        // Handle click on the "Edit" button
         task_edit_el.click(function () {
             const taskIndex = $(this).closest(".task").index();
             const taskToEdit = tasks[taskIndex];
 
+            // Populate the form with the task details
             inputTitle.val(taskToEdit.title);
             inputDescription.val(taskToEdit.description);
             inputCompleteByDate.val(taskToEdit.completeByDate);
 
-            form.data("edit-index", taskIndex); // Store the edit index to update the task later
+            form.data("edit-index", taskIndex);
+
+
             task_edit_el.hide();
+
+            $("#new-task-submit").val("Save Task");
         });
 
+        // Handle click on the "Delete" button
         task_delete_el.click(function () {
             const taskIndex = $(this).closest(".task").index();
+
             $(this).closest(".task").remove();
+
+
             tasks.splice(taskIndex, 1);
+
             updateLocalStorageTasks();
         });
     }
 
+    // Function to update an existing task in the UI
     function updateTask(index, title, description, completeByDate) {
         const taskToEdit = tasks[index];
         taskToEdit.title = title;
         taskToEdit.description = description;
         taskToEdit.completeByDate = completeByDate;
 
+        // Find the task element in the UI and update its content
         const task_element = list_element.children().eq(index);
         const task_content_element = task_element.find(".content");
 
@@ -100,6 +136,7 @@ $(document).ready(function () {
         task_content_element.find("p:eq(1)").text(completeByDate ? "Complete by: " + completeByDate : "");
     }
 
+    // Function to update local storage with the tasks data
     function updateLocalStorageTasks() {
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }
