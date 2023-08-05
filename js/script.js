0d898dd260f933672a5a5f02cf954919a2833b49
Harsh-hy45 <68816204+Harsh-hy45@users.noreply.github.com>
2023-08-06 00:24:34 +0530

    Delete confirmation dialog box added,  addition of tasks in local storage implemented.

--- js/script.js
+++ js/script.js
@@ -6,41 +6,51 @@ const taskList = $("#tasks");
 const openModelButton = $("#open-modal-button");
 const closeModelButton = $("#close-modal-button");
 
+let tasks;
+
+
+let taskToDeleteIndex;
+
+// Open the delete modal when the delete button is clicked
+function openDeleteModal(index) {
+  taskToDeleteIndex = index;
+  $("#delete-modal").css("display", "block");
+}
+
+// Close the delete modal
+function closeDeleteModal() {
+  $("#delete-modal").css("display", "none");
+}
+
 $(document).ready(function () {
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     if (!Array.isArray(tasks)) {
         tasks = [];
-      }
+    }
     openModelButton.click(function () {
         $("#task-modal").css("display", "block");
     });
-
     closeModelButton.click(function () {
         $("#task-modal").css("display", "none");
     });
 
-
     form.submit(function (e) {
         e.preventDefault();
-
         const title = inputTitle.val();
         const description = inputDescription.val();
         const completeByDate = inputCompleteByDate.val();
-
         let formIsValid = validateForm(title, description, completeByDate);
         if (!formIsValid) {
             return;
         }
-
         if (form.data("edit-index") !== undefined) {
             $(".edit").show();
             $("#new-task-submit").val("Add Task");
-
             const editIndex = form.data("edit-index");
             updateTask(editIndex, title, description, completeByDate);
             form.data("edit-index", undefined);
             form.find(".edit").show();
-        } 
+        }
         else {
             addTask(title, description, completeByDate);
             tasks.push({
@@ -48,16 +58,15 @@ $(document).ready(function () {
                 description,
                 completeByDate
             });
-            updateLocalStorageTasks();
+
         }
 
-        
+        updateLocalStorageTasks();
         inputTitle.val("");
         inputDescription.val("");
         inputCompleteByDate.val("");
         $("#task-modal").css("display", "none");
     });
-
 
     $("#new-task-submit").click(function () {
         $("#task-modal").css("display", "none");
@@ -68,20 +77,17 @@ $(document).ready(function () {
     }
 });
 
-
-
 function validateForm(title, description, completeByDate) {
     let formIsValid = true;
-
     if (!validateTitle(title) || !validateDescription(description) || !validateCompleteBy(completeByDate)) {
         formIsValid = false;
     }
-
     return formIsValid;
 }
 
 function addTask(title, description, completeByDate) {
-    const taskElement = $(`
+
+    const taskRow = $(`
     <tr class="task">
     <td>${title}</td>
     <td class="actions">
@@ -91,20 +97,15 @@ function addTask(title, description, completeByDate) {
     </tr>
     `);
 
-    // taskList.append(taskElement);
-    $("#task-table tbody").append(taskElement);
-
+    $("#task-table tbody").append(taskRow);
     const taskEditButton = $("#task-table tbody tr:last-child .edit");
     const taskDeleteButton = $("#task-table tbody tr:last-child .delete");
-
     taskEditButton.click(function () {
         const taskIndex = $(this).closest('.task').index();
         const taskToEdit = tasks[taskIndex];
-
         inputTitle.val(taskToEdit.title);
         inputDescription.val(taskToEdit.description);
         inputCompleteByDate.val(taskToEdit.completeByDate);
-
         form.data('edit-index', taskIndex);
         taskEditButton.hide();
         $('#new-task-submit').val('Save Task');
@@ -112,23 +113,31 @@ function addTask(title, description, completeByDate) {
 
     taskDeleteButton.click(function () {
         const taskIndex = $(this).closest('.task').index();
-
-        $(this).closest('.task').remove();
-        tasks.splice(taskIndex, 1);
-        updateLocalStorageTasks();
+        openDeleteModal(taskIndex);
     });
-     updateLocalStorageTasks();
 }
+
+$("#confirm-delete").click(function () {
+    // Delete the task after confirmation
+    tasks.splice(taskToDeleteIndex, 1);
+    updateLocalStorageTasks();
+    $(".task").eq(taskToDeleteIndex).remove();
+    closeDeleteModal();
+  });
+
+  // Add event listener for the cancel delete button in the modal
+  $("#cancel-delete").click(function () {
+    // Close the delete modal without deleting the task
+    closeDeleteModal();
+  });
 
 function updateTask(index, title, description, completeByDate) {
     const taskToEdit = tasks[index];
     taskToEdit.title = title;
     taskToEdit.description = description;
     taskToEdit.completeByDate = completeByDate;
-
-    const taskElement = taskList.children().eq(index);
-    const taskContentElement = taskElement.find(".content");
-
+    const taskRow = taskList.children().eq(index);
+    const taskContentElement = taskRow.find(".content");
     taskContentElement.find(".task-title").text(title);
     taskContentElement.find("p:eq(0)").text(description ? description : "");
     taskContentElement.find("p:eq(1)").text(completeByDate ? "Complete by: " + completeByDate : "");
@@ -136,14 +145,15 @@ function updateTask(index, title, description, completeByDate) {
 
 function updateLocalStorageTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log("Tasks updated in localStorage");
+    console.log(tasks);
 }
 
 function validateTitle(title) {
     if (title.length === 0) {
         alert("Title is required.");
         return false;
-    } else if (title.length < 3 || title.length > 50) {
+    }
+    else if (title.length < 3 || title.length > 50) {
         alert("Title should be between 3 and 50 characters.");
         return false;
     }
